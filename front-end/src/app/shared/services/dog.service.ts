e29e8471b555c6f867a9b18c8afb61e1e57831f1
duncanchiu409 <chiuduncan409@gmail.com>
2023-04-26 13:34:41 +0930

    created dog service

---
+++ front-end/src/app/shared/services/dog.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { DogInfo } from '../interfaces/dogInfo'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DogService {
+
+  constructor() { }
+
+  
+}
