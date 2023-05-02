e8fe108a677b3fef5eb0918af6fa95a2830ed4fd
duncanchiu409 <chiuduncan409@gmail.com>
2023-05-02 14:28:47 +0930

    add sorting logic

--- front-end/src/app/components/challenges/challenges.component.ts
+++ front-end/src/app/components/challenges/challenges.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Challenge } from 'src/app/shared/interfaces/challenge';
 import { ChallengeService } from 'src/app/shared/services/challenge.service';
 
 @Component({
@@ -7,9 +8,35 @@ import { ChallengeService } from 'src/app/shared/services/challenge.service';
   styleUrls: ['./challenges.component.css']
 })
 export class ChallengesComponent implements OnInit {
-  constructor(public challengeService :ChallengeService) { }
+  public challenges :Challenge[] = []
+
+  constructor(public challengeService :ChallengeService) {
+    this.challenges = [...this.challengeService.challenges]
+  }
 
   ngOnInit(): void {
+    this.challenges = [...this.challengeService.challenges]
+  }
 
+  sortUpComing() :void{
+    this.challenges = this.challengeService.challenges.filter((challenge :Challenge) => {
+      if((new Date(challenge.timer).getTime() - new Date().getTime()) >= 0){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+  }
+
+  sortActive() :void{
+    this.challenges = this.challengeService.challenges.filter((challenge :Challenge) => {
+      if(!((new Date(challenge.timer).getTime() - new Date().getTime()) >= 0)){
+        return true
+      }
+      else{
+        return false
+      }
+    })
   }
 }
