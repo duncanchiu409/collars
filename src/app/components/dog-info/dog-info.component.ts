19e8001f681af90a115cdafb989e32703ba97495
duncanchiu409 <chiuduncan409@gmail.com>
2023-06-20 09:33:15 +0930

    [feature] preview uploaded image from dialog

--- src/app/components/dog-info/dog-info.component.ts
+++ src/app/components/dog-info/dog-info.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { DogService } from 'src/app/shared/services/dog.service';
@@ -18,9 +19,16 @@ export class DogInfoComponent implements OnInit {
   public dogTeritory :string = ''
   public dogTeritoryStatus :boolean = false;
 
-  constructor (public router :Router, private dogSevice :DogService) { }
+  public cropped_profile_image :File | null
+  public original_profile_image :File | null
+
+  constructor (public router :Router, private dogSevice :DogService, public dialog :MatDialog) {
+    this.original_profile_image = null
+    this.cropped_profile_image = null
+  }
 
   ngOnInit(): void {
+
   }
 
   onDone(){
@@ -84,5 +92,49 @@ export class DogInfoComponent implements OnInit {
       // dog.userId = JSON.parse(user).uid
       // this.router.navigate(["/challenges"])
     }
+  }
+
+  openDialog() :void{
+    this.dialog.open(imageDialog, {
+      width: '400px',
+      data: {
+        original: this.original_profile_image,
+        cropped: this.cropped_profile_image
+      }
+    })
+  }
+}
+
+@Component({
+  selector: 'image-dialog',
+  templateUrl: './image-dialog.html',
+})
+export class imageDialog{
+  public original_profile_image :File | null;
+  public cropped_profile_image :File | null;
+  public image_dataURL = ''
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data :any, public dialogRef :MatDialogRef<imageDialog>){
+    this.original_profile_image = data.original
+    this.cropped_profile_image = data.cropped
+  }
+
+  fileUpload(e :any){
+    this.original_profile_image = e.target.files[0]
+
+    const reader = new FileReader()
+    if(reader !== null){
+      reader.onloadend = () => {
+        this.image_dataURL = <string> reader.result
+      }
+    }
+    if(this.original_profile_image !== null){
+      reader.readAsDataURL(this.original_profile_image)
+    }
+    console.log(this.original_profile_image)
+  }
+
+  closeDialog(){
+    this.dialogRef.close()
   }
 }
